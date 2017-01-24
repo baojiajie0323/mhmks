1562a7fed7ccf0dc790c9ed34cba921fb4da8b63
baojiajie0323c <382058502@qq.com>
2017-01-24 13:09:48 +0800

    session

--- mkserver/app.js
+++ mkserver/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'mhmks', cookie: { maxAge: 5000 }}));
 
 app.use('/', routes);
 app.use('/users', users);
