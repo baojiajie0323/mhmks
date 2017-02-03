1ccf66e3091159656c825923aeb01135b429fc70
baojiajie0323c <382058502@qq.com>
2017-02-03 21:52:44 +0800

    首页

--- mkweb/components/main/homepage/plandepart.jsx
+++ mkweb/components/main/homepage/plandepart.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import echarts from 'echarts';
 import styles from './homepage.less';
 
 class Plandepart extends React.Component {
@@ -8,13 +9,68 @@ class Plandepart extends React.Component {
     };
   }
   componentDidMount() {
+    this.initCharts();
   }
   componentWillUnmount() {
   }
+  initCharts() {
+    var myChart = echarts.init(document.getElementById('plandepart'));
+    var option = {
+      tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+      },
+      legend: {
+        orient: 'vertical',
+        x: 'right',
+        data: ['华中区', '华南区', '华北区', '东北区', '沪浙区', '苏皖区']
+      },
+      series: [
+        {
+          name: '当日计划数',
+          type: 'pie',
+          radius: ['50%', '70%'],
+          avoidLabelOverlap: false,
+          label: {
+            normal: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              show: true,
+              textStyle: {
+                fontSize: '30',
+                fontWeight: 'bold'
+              }
+            }
+          },
+          labelLine: {
+            normal: {
+              show: false
+            }
+          },
+          data: [
+            { value: 12, name: '华中区' },
+            { value: 7, name: '华南区' },
+            { value: 8, name: '华北区' },
+            { value: 4, name: '东北区' },
+            { value: 18, name: '沪浙区' },
+            { value: 16, name: '苏皖区' },
+          ]
+        }
+      ],
+      animationEasing: 'elasticOut',
+      animationDelayUpdate: function (idx) {
+        return idx * 5;
+      }
+    };
+    myChart.setOption(option);
+  }
   render() {
     return (
-      <div className={[styles.block,styles.plandepart].join(' ')}>
+      <div className={[styles.block, styles.plandepart].join(' ') }>
         <div className={styles.blocktitle}>部门当日计划</div>
+        <div id='plandepart' className={styles.blockcontent}></div>
       </div>
     );
   }
--- mkweb/components/main/homepage/planpercent_month.jsx
+++ mkweb/components/main/homepage/planpercent_month.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import echarts from 'echarts';
 import styles from './homepage.less';
 
 class PlanpercentMonth extends React.Component {
@@ -8,14 +9,66 @@ class PlanpercentMonth extends React.Component {
     };
   }
   componentDidMount() {
+    this.initCharts();
   }
   componentWillUnmount() {
   }
+  initCharts() {
+    var myChart = echarts.init(document.getElementById('planpercent_month'));
+    var option = {
+      color: ['#3398DB'],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['0201', '0202', '0203', '0204', '0205', '0206', '0207',
+            '0208', '0209', '0210', '0211', '0212', '0213', '0214',
+            '0215', '0216', '0217', '0218', '0219', '0220', '0221'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '计划完成数',
+          type: 'bar',
+          barWidth: '60%',
+          data: [30, 35, 33, 34, 40, 33, 31, 30, 35, 33, 34, 40, 33, 31, 30, 35, 33, 34, 40, 33, 31],
+          animationDelay: function (idx) {
+            return idx * 10;
+          }
+        }
+      ],
+      animationEasing: 'elasticOut',
+      animationDelayUpdate: function (idx) {
+        return idx * 5;
+      }
+    };
+    myChart.setOption(option);
+  }
   render() {
     return (
-      <div className={[styles.block,styles.planpercent_month].join(' ')}>
-        <div className={styles.blocktitle}>当月计划概览</div>
-        
+      <div className={[styles.block, styles.planpercent_month].join(' ') }>
+        <div className={styles.blocktitle}>当月每日计划完成数</div>
+        <div id='planpercent_month' className={styles.blockcontent}></div>
+
       </div>
     );
   }
