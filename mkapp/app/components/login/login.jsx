c5e6ca26ac32fc5124cf6b8ab0eba1cb18d86e1f
baojiajie0323 <bjj@BJJ-PC.kdcrd.com>
2017-02-13 17:27:05 +0800

    登录界面修改

--- mkapp/app/components/login/login.jsx
+++ mkapp/app/components/login/login.jsx
@@ -20,41 +20,10 @@ class Login extends React.Component {
   }
 
   componentDidMount() {
-    // this.map = new BMap.Map("allmap");
-    // var point = new BMap.Point(116.331398, 39.897445);
-    // this.map.centerAndZoom(point, 12);
-    // var context = this;
-    // var geolocation = new BMap.Geolocation();
-    // geolocation.getCurrentPosition(function (r) {
-    //   if (this.getStatus() == BMAP_STATUS_SUCCESS) {
-    //     var mk = new BMap.Marker(r.point);
-    //     context.map.addOverlay(mk);
-    //     context.map.panTo(r.point);
-    //     alert('您的位置：' + r.point.lng + ',' + r.point.lat);
-    //   }
-    //   else {
-    //     alert('failed' + this.getStatus());
-    //   }
-    // }, { enableHighAccuracy: true })
   }
   componentWillUnmount() {
     localStorage.username = $('#username').val();
     localStorage.password = $('#password').val();
-  }
-  onTouchTest() {
-    var context = this;
-    var geolocation = new BMap.Geolocation();
-    geolocation.getCurrentPosition(function (r) {
-      if (this.getStatus() == BMAP_STATUS_SUCCESS) {
-        var mk = new BMap.Marker(r.point);
-        context.map.addOverlay(mk);
-        context.map.panTo(r.point);
-        alert('您的位置：' + r.point.lng + ',' + r.point.lat);
-      }
-      else {
-        alert('failed' + this.getStatus());
-      }
-    }, { enableHighAccuracy: true })
   }
 
   onClickLogin() {
@@ -76,7 +45,6 @@ class Login extends React.Component {
     Action.login(data);
   }
   handleTouchTap() {
-    alert('onTouchTap triggered on the title component');
   }
   render() {
     var username = localStorage.username;
@@ -91,10 +59,10 @@ class Login extends React.Component {
           onRightIconButtonTouchTap={this.handleTouchTap}
           />
         <div className={styles.inputform}>
-          <TextField defaultValue={username} id="username" fullWidth={true} hintText="请输入用户名" />
+          <TextField floatingLabelText="用户名" defaultValue={username} id="username" fullWidth={true} />
         </div>
         <div className={styles.inputform}>
-          <TextField defaultValue={password} id="password" type="password" fullWidth={true} hintText="请输入密码" />
+          <TextField floatingLabelText="密码" defaultValue={password} id="password" type="password" fullWidth={true} hintText="请输入密码" />
         </div>
         <div className={styles.btnform}>
           <RaisedButton onTouchTap={this.onClickLogin} labelStyle={{fontSize:'16px'}} buttonStyle={{height:'50px'}} label="登 录" primary={true} fullWidth={true} />
