cf37f988e7ca0e1d3338030e3f0377466198413c
baojiajie0323 <382058502@qq.com>
2017-05-18 19:57:03 +0800

    补贴标准修改

--- mkweb/components/main/config/subsidy.jsx
+++ mkweb/components/main/config/subsidy.jsx
@@ -16,21 +16,19 @@ class Subsidy extends React.Component {
     this.subsidyInfo = {
       jt1: { name: '交通工具 500公里以上(含)' },
       jt2: { name: '交通工具 500公里以下' },
-      gzdcf1: { name: '工作地餐费 一线城市' },
-      gzdcf2: { name: '工作地餐费 二线城市' },
-      gzdcf3: { name: '工作地餐费 三线城市' },
+      gzdcf1: { name: '误餐标准 一线城市' },
+      gzdcf2: { name: '误餐标准 二线城市' },
+      gzdcf3: { name: '误餐标准 三线城市' },
       gzdjt1: { name: '工作地交通 一线城市' },
       gzdjt2: { name: '工作地交通 二线城市' },
       gzdjt3: { name: '工作地交通 三线城市' },
-      cczs1: { name: '出差住宿 一线城市' },
-      cczs2: { name: '出差住宿 二线城市' },
-      cczs3: { name: '出差住宿 三线城市' },
-      ccjt1: { name: '出差交通 一线城市' },
-      ccjt2: { name: '出差交通 二线城市' },
-      ccjt3: { name: '出差交通 三线城市' },
+      ccjt1: { name: '出差地交通 一线城市' },
+      ccjt2: { name: '出差地交通 二线城市' },
+      ccjt3: { name: '出差地交通 三线城市' },
       ccbt1: { name: '出差补贴 一线城市' },
       ccbt2: { name: '出差补贴 二线城市' },
       ccbt3: { name: '出差补贴 三线城市' },
+      txf: { name: '通讯费补贴' }
     }
     this.handleOk = this.handleOk.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
@@ -66,14 +64,14 @@ class Subsidy extends React.Component {
       key: this._cate,
       value: this.state.modalvalue,
     }
-    console.log("updateSubsidy",data);
+    console.log("updateSubsidy", data);
     Action.updateSubsidy(data);
   }
   handleCancel() {
     this.setState({ visible: false })
   }
 
-  onClickText(text,record,cate) {
+  onClickText(text, record, cate) {
     this._curRecord = record;
     this._cate = cate;
     this.setState({
@@ -110,7 +108,7 @@ class Subsidy extends React.Component {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text,record, 'jt1') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'jt1') } } >{text}</p>
             }
           }, {
             title: <p style={{ textAlign: 'center' }}>500公里以下</p>,
@@ -122,215 +120,186 @@ class Subsidy extends React.Component {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text,record, 'jt2') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'jt2') } } >{text}</p>
             }
           },
         ]
       }, {
-        title: <p style={{ color: 'rgb(16,142,233)' }}>工作地 室内餐费标准</p>,
+        title: <p>误餐标准</p>,
         children: [
           {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>一线城市 (元/日) </p>,
+            title: <p style={{textAlign: 'center' }}>一线城市 (元/日) </p>,
             dataIndex: 'gzdcf1',
             key: 'gzdcf1',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'gzdcf1') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'gzdcf1') } } >{text}</p>
             }
           }, {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>二线城市 (元/日) </p>,
+            title: <p style={{textAlign: 'center' }}>二线城市 (元/日) </p>,
             dataIndex: 'gzdcf2',
             key: 'gzdcf2',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text,record, 'gzdcf2') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'gzdcf2') } } >{text}</p>
             }
           }, {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>三线城市 (元/日) </p>,
+            title: <p style={{ textAlign: 'center' }}>三线城市 (元/日) </p>,
             dataIndex: 'gzdcf3',
             key: 'gzdcf3',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'gzdcf3') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'gzdcf3') } } >{text}</p>
             }
           }
         ]
       }, {
-        title: <p style={{ color: 'rgb(16,142,233)' }}>工作地 室内交通标准</p>,
+        title: <p>工作地交通费标准</p>,
         children: [
           {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>一线城市 (元/日) </p>,
+            title: <p style={{ textAlign: 'center' }}>一线城市 (元/日) </p>,
             dataIndex: 'gzdjt1',
             key: 'gzdjt1',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'gzdjt1') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'gzdjt1') } } >{text}</p>
             }
           }, {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>二线城市 (元/日) </p>,
+            title: <p style={{ textAlign: 'center' }}>二线城市 (元/日) </p>,
             dataIndex: 'gzdjt2',
             key: 'gzdjt2',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'gzdjt2') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'gzdjt2') } } >{text}</p>
             }
           }, {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>三线城市 (元/日) </p>,
+            title: <p style={{ textAlign: 'center' }}>三线城市 (元/日) </p>,
             dataIndex: 'gzdjt3',
             key: 'gzdjt3',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'gzdjt3') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'gzdjt3') } } >{text}</p>
             }
           }
         ]
       }, {
-        title: <p style={{ color: 'rgb(62, 134, 85)' }}>出差 住宿标准</p>,
+        title: <p>出差地交通费标准</p>,
         children: [
           {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>一线城市 (元/日) </p>,
-            dataIndex: 'cczs1',
-            key: 'cczs1',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'cczs1') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>二线城市 (元/日) </p>,
-            dataIndex: 'cczs2',
-            key: 'cczs2',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text,record, 'cczs2') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>三线城市 (元/日) </p>,
-            dataIndex: 'cczs3',
-            key: 'cczs3',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text,record,'cczs3') } } >{text}</p>
-            }
-          }
-        ]
-      }, {
-        title: <p style={{ color: 'rgb(62, 134, 85)' }}>出差 交通报销标准</p>,
-        children: [
-          {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>一线城市 (元/日) </p>,
+            title: <p style={{ textAlign: 'center' }}>一线城市 (元/日) </p>,
             dataIndex: 'ccjt1',
             key: 'ccjt1',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text,record, 'ccjt1') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'ccjt1') } } >{text}</p>
             }
           }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>二线城市 (元/日) </p>,
+            title: <p style={{ textAlign: 'center' }}>二线城市 (元/日) </p>,
             dataIndex: 'ccjt2',
             key: 'ccjt2',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'ccjt2') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'ccjt2') } } >{text}</p>
             }
           }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>三线城市 (元/日) </p>,
+            title: <p style={{ textAlign: 'center' }}>三线城市 (元/日) </p>,
             dataIndex: 'ccjt3',
             key: 'ccjt3',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'ccjt3') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'ccjt3') } } >{text}</p>
             }
           }
         ]
       }, {
-        title: <p style={{ color: 'rgb(62, 134, 85)' }}>出差 补贴标准</p>,
+        title: <p>出差 补贴标准</p>,
         children: [
           {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>一线城市 (元/日) </p>,
+            title: <p style={{textAlign: 'center' }}>一线城市 (元/日) </p>,
             dataIndex: 'ccbt1',
             key: 'ccbt1',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'ccbt1') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'ccbt1') } } >{text}</p>
             }
           }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>二线城市 (元/日) </p>,
+            title: <p style={{ textAlign: 'center' }}>二线城市 (元/日) </p>,
             dataIndex: 'ccbt2',
             key: 'ccbt2',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'ccbt2') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'ccbt2') } } >{text}</p>
             }
           }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>三线城市 (元/日) </p>,
+            title: <p style={{ textAlign: 'center' }}>三线城市 (元/日) </p>,
             dataIndex: 'ccbt3',
             key: 'ccbt3',
-            width: 70,
+            width: 65,
             render: function (text, record) {
               if (!text) {
                 text = "无数据";
               }
               return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record,'ccbt3') } } >{text}</p>
+                onClick={function () { context.onClickText(text, record, 'ccbt3') } } >{text}</p>
             }
           }
         ]
+      }, {
+        title: <p>通讯费 (元/月)</p>,
+        dataIndex: 'txf',
+        key: 'txf',
+        width: 60,
+        render: function (text, record) {
+          if (!text) {
+            text = "无数据";
+          }
+          return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
+            onClick={function () { context.onClickText(text, record, 'txf') } } >{text}</p>
+        }
       }];
   }
   getTableData() {
@@ -344,8 +313,8 @@ class Subsidy extends React.Component {
       <div className={styles.configcontent}>
         <p className={styles.configtitle}>补贴报销标准</p>
         <div className={styles.configtable}>
-          <Table size="small" loading={this.state.loading} bordered scroll={{ x: 1500 }}
-            columns={this.getTableColumn() } dataSource={this.getTableData() } />
+          <Table size="small" loading={this.state.loading} bordered scroll={{ x: 1285 }}
+            columns={this.getTableColumn()} dataSource={this.getTableData()} />
         </div>
         <Modal width={350} title={this.state.modaltitle} visible={this.state.visible}
           onOk={this.handleOk} onCancel={this.handleCancel}
