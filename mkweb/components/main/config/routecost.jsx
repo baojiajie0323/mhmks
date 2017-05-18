160654e87f718d10677965b5348a4bd5feb8227d
baojiajie0323 <382058502@qq.com>
2017-05-18 13:52:37 +0800

    98

--- mkweb/components/main/config/routecost.jsx
+++ mkweb/components/main/config/routecost.jsx
@@ -15,11 +15,11 @@ class Routecost extends React.Component {
       modalvalue: '',
     };
     this.routetype = [{
-      type:1,
-      name:"所辖门店"
-    },{
-      type:2,
-      name:"稽核门店"
+      type: 1,
+      name: "所辖门店"
+    }, {
+      type: 2,
+      name: "稽核门店"
     }];
     this.handleOk = this.handleOk.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
@@ -71,255 +71,109 @@ class Routecost extends React.Component {
       modalvalue: text,
     })
   }
+  getDepartOption() {
+    return null;
+  }
   getTableColumn() {
     var context = this;
     return [
       {
-        title: <p style={{ textAlign: 'center' }}>岗位等级</p>,
+        title: <p style={{ textAlign: 'center' }}>路线</p>,
         dataIndex: 'level',
         key: 'level',
         width: 65,
         fixed: 'left'
       }, {
-        title: <p style={{ textAlign: 'center' }}>适用岗位</p>,
+        title: <p style={{ textAlign: 'center' }}>负责人</p>,
         dataIndex: 'name',
         key: 'name',
         width: 80,
         fixed: 'left'
       }, {
-        title: '交通工具类别',
-        children: [
-          {
-            title: <p style={{ textAlign: 'center' }}>500公里以上(含) </p>,
-            dataIndex: 'jt1',
-            key: 'jt1',
-            width: 120,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'jt1') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ textAlign: 'center' }}>500公里以下</p>,
-            dataIndex: 'jt2',
-            key: 'jt2',
-            width: 180,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'jt2') } } >{text}</p>
-            }
-          },
-        ]
+        title: <p style={{ textAlign: 'center' }}>岗位</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
       }, {
-        title: <p style={{ color: 'rgb(16,142,233)' }}>工作地 室内餐费标准</p>,
-        children: [
-          {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>一线城市 (元/日) </p>,
-            dataIndex: 'gzdcf1',
-            key: 'gzdcf1',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'gzdcf1') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>二线城市 (元/日) </p>,
-            dataIndex: 'gzdcf2',
-            key: 'gzdcf2',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'gzdcf2') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>三线城市 (元/日) </p>,
-            dataIndex: 'gzdcf3',
-            key: 'gzdcf3',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'gzdcf3') } } >{text}</p>
-            }
-          }
-        ]
+        title: <p style={{ textAlign: 'center' }}>门店名称</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
       }, {
-        title: <p style={{ color: 'rgb(16,142,233)' }}>工作地 室内交通标准</p>,
-        children: [
-          {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>一线城市 (元/日) </p>,
-            dataIndex: 'gzdjt1',
-            key: 'gzdjt1',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'gzdjt1') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>二线城市 (元/日) </p>,
-            dataIndex: 'gzdjt2',
-            key: 'gzdjt2',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'gzdjt2') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(16,142,233)', textAlign: 'center' }}>三线城市 (元/日) </p>,
-            dataIndex: 'gzdjt3',
-            key: 'gzdjt3',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'gzdjt3') } } >{text}</p>
-            }
-          }
-        ]
+        title: <p style={{ textAlign: 'center' }}>门店地址</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
       }, {
-        title: <p style={{ color: 'rgb(62, 134, 85)' }}>出差 住宿标准</p>,
-        children: [
-          {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>一线城市 (元/日) </p>,
-            dataIndex: 'cczs1',
-            key: 'cczs1',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'cczs1') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>二线城市 (元/日) </p>,
-            dataIndex: 'cczs2',
-            key: 'cczs2',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'cczs2') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>三线城市 (元/日) </p>,
-            dataIndex: 'cczs3',
-            key: 'cczs3',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'cczs3') } } >{text}</p>
-            }
-          }
-        ]
+        title: <p style={{ textAlign: 'center' }}>城市等级</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
       }, {
-        title: <p style={{ color: 'rgb(62, 134, 85)' }}>出差 交通报销标准</p>,
-        children: [
-          {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>一线城市 (元/日) </p>,
-            dataIndex: 'ccjt1',
-            key: 'ccjt1',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'ccjt1') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>二线城市 (元/日) </p>,
-            dataIndex: 'ccjt2',
-            key: 'ccjt2',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'ccjt2') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>三线城市 (元/日) </p>,
-            dataIndex: 'ccjt3',
-            key: 'ccjt3',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'ccjt3') } } >{text}</p>
-            }
-          }
-        ]
+        title: <p style={{ textAlign: 'center' }}>路线描述</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
       }, {
-        title: <p style={{ color: 'rgb(62, 134, 85)' }}>出差 补贴标准</p>,
-        children: [
-          {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>一线城市 (元/日) </p>,
-            dataIndex: 'ccbt1',
-            key: 'ccbt1',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'ccbt1') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>二线城市 (元/日) </p>,
-            dataIndex: 'ccbt2',
-            key: 'ccbt2',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'ccbt2') } } >{text}</p>
-            }
-          }, {
-            title: <p style={{ color: 'rgb(62, 134, 85)', textAlign: 'center' }}>三线城市 (元/日) </p>,
-            dataIndex: 'ccbt3',
-            key: 'ccbt3',
-            width: 70,
-            render: function (text, record) {
-              if (!text) {
-                text = "无数据";
-              }
-              return <p style={{ textAlign: "center", color: "rgb(16,142,233)", cursor: 'pointer' }}
-                onClick={function () { context.onClickText(text, record, 'ccbt3') } } >{text}</p>
-            }
-          }
-        ]
+        title: <p style={{ textAlign: 'center' }}>交通工具</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>交通班次</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>出发时间</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>到达时间</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>长途交通费</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>住宿酒店</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>住宿费</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>到达时间</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>到达时间</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>到达时间</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>到达时间</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
+      }, {
+        title: <p style={{ textAlign: 'center' }}>到达时间</p>,
+        dataIndex: 'name',
+        key: 'name',
+        width: 80,
       }];
   }
   getTableData() {
@@ -333,17 +187,13 @@ class Routecost extends React.Component {
       <div className={styles.configcontent}>
         <p className={styles.configtitle}>路线费用标准</p>
         <div className={styles.editcontent}>
-          <Select defaultValue="" style={{ width: 120,marginRight:'20px'}}>
-            <Option value="jack">Jack</Option>
-            <Option value="lucy">Lucy</Option>
-            <Option value="disabled" disabled>Disabled</Option>
-            <Option value="Yiminghe">yiminghe</Option>
+          <Select defaultValue={1} style={{ width: 120, marginRight: '10px' }}>
+            {this.routetype.map((rt) => {
+              return <Option value={rt.type}>{rt.name}</Option>
+            })}
           </Select>
-          <Select defaultValue="lucy" style={{ width: 120 }}>
-            <Option value="jack">Jack</Option>
-            <Option value="lucy">Lucy</Option>
-            <Option value="disabled" disabled>Disabled</Option>
-            <Option value="Yiminghe">yiminghe</Option>
+          <Select defaultValue={0} style={{ width: 120 }}>
+            {this.getDepartOption()}
           </Select>
         </div>
         <div className={styles.configtable}>
