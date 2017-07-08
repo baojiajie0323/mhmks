a654f4f1097be6382ce5357ed8aa00791a4ed79c
baojiajie0323 <382058502@qq.com>
2017-07-08 10:42:07 +0800

    报销费用

--- mkapp/app/components/main/home/expense.jsx
+++ mkapp/app/components/main/home/expense.jsx
@@ -1,46 +1,49 @@
 import React from 'react';
 import styles from './home.less';
 
-
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
-import { message } from 'antd';
+import { message,Table } from 'antd';
 import LeftIcon from 'material-ui/svg-icons/navigation/chevron-left';
 import TextField from 'material-ui/TextField';
-
+import Subheader from 'material-ui/Subheader';
 import config from '../../config';
 
 class Expense extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      plan: Store.getPlan(),
     };
     this.onClickBack = this.onClickBack.bind(this);
   }
 
   componentDidMount() {
+    var curDate = Store.getCurDate();
   }
   componentWillUnmount() {
   }
   onClickBack() {
     Store.emit(StoreEvent.SE_VIEW, '');
   }
+
   render() {
     const { finished, stepIndex } = this.state;
     return (
       <div className={styles.container}>
         <AppBar
           style={{ paddingTop: config.titlebarPadding }}
-          title='日报'
+          title='费用报销'
           onLeftIconButtonTouchTap={this.onClickBack}
           iconElementLeft={<IconButton><LeftIcon /></IconButton>}
           iconElementRight={<FlatButton label="提交" />}
         />
         <div style={{ top: config.contentTop }} className={styles.content}>
           <div style={{ padding: '0 40px' }}>
-
+            <Subheader>今日拜访情况</Subheader>
+            <Subheader>报销费用填报</Subheader>
           </div>
         </div>
       </div>
