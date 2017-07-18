25a460ba50960bd43768c0327ef16ab3bc59ca89
baojiajie0323 <382058502@qq.com>
2017-07-18 13:00:29 +0800

    费用APP

--- mkapp/app/components/main/home/expense.jsx
+++ mkapp/app/components/main/home/expense.jsx
@@ -3,9 +3,11 @@ import styles from './home.less';
 
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
 import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
-import { message,Table } from 'antd';
+import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
+import { message, Table } from 'antd';
 import LeftIcon from 'material-ui/svg-icons/navigation/chevron-left';
 import TextField from 'material-ui/TextField';
 import Subheader from 'material-ui/Subheader';
@@ -31,6 +33,20 @@ class Expense extends React.Component {
 
   render() {
     const { finished, stepIndex } = this.state;
+    var expenseStyle = {
+      paperStyle: {
+        positon: 'relative',
+        padding: '0 15px',
+        height: '120px',
+        margin: '5px 15px 10px'
+      },
+      titleStyle: {
+        position: 'absolute',
+        left: '25px',
+        marginTop: '4px',
+        color: 'rgb(56, 150, 161)'
+      }
+    }
     return (
       <div className={styles.container}>
         <AppBar
@@ -41,10 +57,43 @@ class Expense extends React.Component {
           iconElementRight={<FlatButton label="提交" />}
         />
         <div style={{ top: config.contentTop }} className={styles.content}>
-          <div style={{ padding: '0 40px' }}>
-            <Subheader>今日拜访情况</Subheader>
-            <Subheader>报销费用填报</Subheader>
-          </div>
+          <Subheader>今日拜访类型</Subheader>
+          <RadioButtonGroup style={{ padding: '0 15px' }} name="shipSpeed" defaultSelected={1}>
+            <RadioButton
+              value={1}
+              label="室内拜访"
+              style={{ marginBottom: "16px" }}
+            />
+            <RadioButton
+              value={2}
+              label="出差住宿"
+              style={{ marginBottom: "16px" }}
+            />
+            <RadioButton
+              value={3}
+              label="出差不住宿"
+              style={{ marginBottom: "16px" }}
+            />
+          </RadioButtonGroup>
+          <Subheader>今日费用申请</Subheader>
+          <Paper style={expenseStyle.paperStyle} zDepth={1}>
+            <p style={expenseStyle.titleStyle}>误餐费</p>
+          </Paper>
+          <Paper style={expenseStyle.paperStyle} zDepth={1}>
+            <p style={expenseStyle.titleStyle}>市内交通费</p>
+          </Paper>
+          <Paper style={expenseStyle.paperStyle} zDepth={1}>
+            <p style={expenseStyle.titleStyle}>出差补贴</p>
+          </Paper>
+          <Paper style={expenseStyle.paperStyle} zDepth={1}>
+            <p style={expenseStyle.titleStyle}>出差地交通费</p>
+          </Paper>
+          <Paper style={expenseStyle.paperStyle} zDepth={1}>
+            <p style={expenseStyle.titleStyle}>长途交通费</p>
+          </Paper>
+          <Paper style={expenseStyle.paperStyle} zDepth={1}>
+            <p style={expenseStyle.titleStyle}>住宿补贴</p>
+          </Paper>
         </div>
       </div>
     );
